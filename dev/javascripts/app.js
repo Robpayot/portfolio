aef25af34fd6a324b01968457051011c979d24fa
Robin Payot <robinpayot@MacBook-Pro-de-AntoineA.local>
2017-01-30 12:38:40 +0100

    init boiler

---
+++ dev/javascripts/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Default Babel Polyfill, be careful, be sure you need it
+// because it's ~6000 lines of code unminified
+// import 'babel-polyfill';
+import 'modernizr';
+// import * as tools from '@84paris/84.tools';
+
+// Your imports
+// import MyComponent from './component.es6'
+console.log('%c 84.Boilerplate ===== Your app is ready.', 'background: #000; color: #FFF');
