544895cc2a7d9e28a3e593e1711fdd0f84e4dc60
Robin Payot <robpayot@gmail.com>
2017-03-26 18:42:43 +0200

    Set up orbit control

--- dev/javascripts/components/Graphic3D.js
+++ dev/javascripts/components/Graphic3D.js
@@ -1,5 +1,10 @@
+import { WebGLRenderer, PerspectiveCamera, Scene, Mesh, SphereGeometry, MeshLambertMaterial, PointLight, Color, MeshBasicMaterial } from 'three';
+import OrbitControls from '../vendors/OrbitControls';
 import EmitterManager from '../managers/EmitterManager';
 import SoundManager from '../managers/SoundManager';
+
+console.log(OrbitControls);
+
 
 export default class Graphic3D {
 
@@ -7,6 +12,7 @@ export default class Graphic3D {
 
         this.raf = this.raf.bind(this);
         this.events = this.events.bind(this);
+        this.setElements = this.setElements.bind(this);
         this.resizeHandler = this.resizeHandler.bind(this);
 
         this.start();
@@ -18,22 +24,62 @@ export default class Graphic3D {
 
         console.log('test 2', SoundManager);
 
+        // Set the scene size.
+        this.width = window.innerWidth;
+        this.height = 500;
+
+
+        // Set some camera attributes.
+        this.viewAngle = 45;
+        this.aspect = this.width / this.height;
+        this.near = 0.1;
+        this.far = 10000;
+
+        // Get the DOM element to attach to
+        this.el = document.querySelector('.graphic3D');
+
+        // Create a WebGL renderer, camera
+        // and a scene
+        this.renderer = new WebGLRenderer({
+            antialias: true,
+        });
+        this.camera =
+            new PerspectiveCamera(
+                this.viewAngle,
+                this.aspect,
+                this.near,
+                this.far
+            );
+
+        this.camera.position.z = 500;
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+        console.log(OrbitControls);
+        // this.controls.addEventListener('change', this.raf); // remove when using animation loop
+        // enable animation loop when using damping or autorotation
+        //controls.enableDamping = true;
+        //controls.dampingFactor = 0.25;
+        this.controls.enableZoom = false;
+
+        this.scene = new Scene();
+        this.scene.background = new Color(0xffffff);
+        console.log(this.scene);
+
+        // Add the camera to the scene.
+        this.scene.add(this.camera);
+
+        // Start the renderer.
+        this.renderer.setSize(this.width, this.height);
+
+        // Attach the renderer-supplied
+        // DOM element.
+        this.el.appendChild(this.renderer.domElement);
+
+        this.elements = [];
+
+        this.setElements();
+        this.setLight();
+
         this.events(true);
-
-
-        return false;
-
-        this.el = document.querySelector('.xp');
-
-
-        this.ui = {
-            myAudio: this.el.querySelector('audio'),
-            formFtt: this.el.querySelector('.formFtt'),
-            frequencies: this.el.querySelector('.frequencies'),
-            hight: this.el.querySelector('.frequencies .hight .circle'),
-            medium: this.el.querySelector('.frequencies .medium .circle'),
-            low: this.el.querySelector('.frequencies .low .circle')
-        };
 
 
 
@@ -49,12 +95,47 @@ export default class Graphic3D {
 
     }
 
+    setElements() {
+
+        // // Set up the sphere vars
+        const RADIUS = 50;
+        const SEGMENTS = 32;
+        const RINGS = 32;
+
+        const geometry = new SphereGeometry(RADIUS, SEGMENTS, RINGS);
+        const material = new MeshLambertMaterial({ color: 0xCC0000 });
+        const sphere = new Mesh(geometry, material);
+        sphere.position.z = -300;
+        this.scene.add(sphere);
+    }
+
+    setLight() {
+        // create a point light
+        const pointLight = new PointLight(0xFFFFFF);
+
+        // set its position
+        pointLight.position.x = 100;
+        pointLight.position.y = 150;
+        pointLight.position.z = 130;
+
+        // Light shadow
+        // pointLight.shadow.radius = 0.5;
+        // pointLight.shadow.bias = 0.5;
+        console.log(pointLight.shadow);
+
+        // add to the scene
+        this.scene.add(pointLight);
+    }
 
     resizeHandler() {
         this.canvas.width = window.innerWidth;
     }
 
     raf() {
+
+    	this.controls.update();
+        // Draw!
+        this.renderer.render(this.scene, this.camera);
 
 
     }
